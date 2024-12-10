ad47cce840a95233d1ea5a9eabbf080d864e9aec
dalsadakah <danialsadakah@outlook.com>
2024-12-10 18:24:27 +0700

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 window._config = {
     cognito: {
-        userPoolId: 'eu-central-1_Vi76rxPcV', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '48l4h1uhntiihpe90dp0dk81cg', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolId: 'eu-central-1_BP5LopM7M', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '7n2dqneem3rimnffd3001u0g7i', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: 'eu-central-1' // e.g. us-east-2
     },
     api: {
